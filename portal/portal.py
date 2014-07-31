84c4e77f58f58c4d20c652e833ad36d830242561
Adolfo <adf.melo@gmail.com>
2014-07-31 09:54:29 -0300

    Update portal.py

--- portal/portal.py
+++ portal/portal.py
@@ -5,60 +5,59 @@ import glob
 import os
 import sqlite3
 #
-#  file paths
+#   file paths
 #
 db_filename = 'db/portal.db'
 schema_filename = 'db/portal_schema.sql'
 #
-#  keywords
+#   keywords
 #
 MASTER_KEY = '0a 14 68 a1'
 ALLOWED, DENIED, CHECKER, PORT, BAUD_RATE = 'a', 'b', 'c', '/dev/ttyACM1', 9600
 #
-#  variables
+#   variables
 #
 updating_list = False
 authorized_uids = []
 #
-#  functions
+#   functions
 #
 def list_update():                              # Alternates Boolean global variable
     global updating_list
     updating_list = not updating_list
 
 def update_list(uid):
-    if not MASTER_KEY in uid:                   # ignores updating if master key is present
-        if uid in authorized_uids:              # check if uid is on the list
-            rm_uid(uid)                         # remove it if it's there
+    if not MASTER_KEY in uid:                   # Ignores updating if master key is present
+        if uid in authorized_uids:              # Checks if UID is on the list
+            rm_uid(uid)                         # Removes UID if it's there
             print(uid + ' removed')
             arduino.write(DENIED)
-        elif not MASTER_KEY in uid:
-            authorized_uids.append(uid)
-            print(uid + ' added')
-            add_uid(uid)                        # saves the new uid into database
+        else:                               	# Adds the UID if it's not there yet
+            add_uid(uid)                        # Saves the new uid into database and in local memory
             arduino.write(ALLOWED)
+            print(uid + ' added')
         list_update()
 
-def load_uids():                                # checks database and loads all uids that are already there
+def load_uids():                                # Checks database and loads all uids already there
 
-    sql = 'select uid from card'                # selects uid column
+    sql = 'select uid from card'                # Selects uid column
     conn.row_factory = sqlite3.Row
     conn.text_factory = str
     cursor = conn.cursor()
     cursor.execute(sql)
-    for record in cursor.fetchall():
+    for record in cursor.fetchall():			# Iterates over column rows
         print(record[0])
         authorized_uids.append(record[0])
 
-def add_uid(uid):                               # add it if it isn't        # UPDATE PASSED PARAMETRS
+def add_uid(uid):                               # Adds uid from local memory and database      # UPDATE PASSED PARAMETRS
     sql = 'insert into card values (?,?,?)'
     conn = sqlite3.connect(db_filename)
     cursor = conn.cursor()
-    cursor.execute(sql, (uid, 'card_name', 'card_mail'))
+    cursor.execute(sql, (uid, 'no_card_name_associated', 'no_card_mail_associated'))
     conn.commit()
     authorized_uids.append(uid)
 
-def rm_uid(uid):                                # REMOVES UID
+def rm_uid(uid):                                # Removes uid from local memory and database
     conn = sqlite3.connect(db_filename)
     cursor = conn.cursor()
     conn.text_factory = str    
@@ -68,12 +67,12 @@ def rm_uid(uid):                                # REMOVES UID
     conn.commit()
     authorized_uids.remove(uid)
 
-def scan_ports():                               # gets all ttyACM accessible ports (Linux)
+def scan_ports():                               # Gets all ttyACM accessible ports (Linux)
     return glob.glob('/dev/ttyACM*')
 #
-#   DATABASE CONFIG
+#   Database configuration
 #
-db_is_new = not os.path.exists(db_filename).    # checks if database file is there
+db_is_new = not os.path.exists(db_filename).    # Checks if database file is there
 
 with sqlite3.connect(db_filename) as conn:
     if db_is_new:
@@ -96,13 +95,13 @@ with sqlite3.connect(db_filename) as conn:
     else:
         print 'Database exists, assume schema does, too.'
 #
-#  Arduino conection
+#   Arduino conection
 #
 print "available ttyACM ports:"
 print scan_ports()
 print '---\n---'
 
-for port in scan_ports():
+for port in scan_ports():						# Tries to connect on the ports available (it will connect to the last one)					
     try:
         arduino = serial.Serial(port, BAUD_RATE)
         print "conected to port " + port
@@ -110,31 +109,31 @@ for port in scan_ports():
         print "failed to conect on " + port
 
 print "authorized uids:"
-load_uids()                                   # loads already registered uids on memory
+load_uids()                                   	# Loads already registered uids on memory
 print '---\n---'
 
-time.sleep(2)                                 # waiting arduino's initialization...
+time.sleep(2		                            # Waiting arduino's initialization...
 print(" Portal ready.\n---")
 #
-#  loop
+#   loop
 #
-while 1:                                      # main loop
+while 1:                                      # Main loop
     if arduino.inWaiting() > 0:
-        uid = arduino.readline().strip()      # reads what arduino has written
-        if '<' in uid and '>' in uid :        # verifies if a UID was readden
+        uid = arduino.readline().strip()      # Reads what arduino has written
+        if '<' in uid and '>' in uid :        # Verifies if a UID was readden
 
-            uid = uid[2:-2]                   # remove < > simbols
+            uid = uid[2:-2]                   # Remove < > simbols
 
             if updating_list:
                 update_list(uid)
 
             elif MASTER_KEY in uid:           # MASTER_KEY CHECKER
-                arduino.write(CHECKER)        # master_key signal on arduino
+                arduino.write(CHECKER)        # Master_key signal on arduino
                 print("MASTER_KEY mode")
                 list_update()
 
-            elif uid in authorized_uids:      # check if uid is in the list here
-                arduino.write(ALLOWED)        # is there
+            elif uid in authorized_uids:      # Check if uid is in the list here
+                arduino.write(ALLOWED)        # Is there
                 print("UID " + uid + " allowed")
 
             else:                             # is not there
